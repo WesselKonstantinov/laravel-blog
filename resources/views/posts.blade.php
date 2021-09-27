794c1860fa17c80c6b42ac108a1bd875141973ab
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-09-27 21:02:19 +0200

    Use blade layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,16 @@
-<!DOCTYPE html>
+@extends('layout')
 
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-
-<body>
-    @foreach ($posts as $post)
-    <article>
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        <div>
-            {{ $post->excerpt }}
-        </div>
-    </article>
-    @endforeach
-</body>
+@section('content')
+@foreach ($posts as $post)
+<article>
+    <h1>
+        <a href="/posts/{{ $post->slug }}">
+            {{ $post->title }}
+        </a>
+    </h1>
+    <div>
+        {{ $post->excerpt }}
+    </div>
+</article>
+@endforeach
+@endsection
