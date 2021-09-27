5154c510111a79ba6f50e268d3025f2c862ad564
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-09-27 20:23:22 +0200

    Sort posts and update cache

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
-{   
+{
     public $title;
 
     public $excerpt;
@@ -29,15 +29,18 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path("posts")))
-            ->map(fn($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn($document) => new Post(
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts")))
+                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn ($document) => new Post(
                     $document->title,
                     $document->excerpt,
                     $document->date,
                     $document->body(),
                     $document->slug
-                ));
+                ))
+                ->sortByDesc('date');
+        });
     }
 
     public static function find($slug)
