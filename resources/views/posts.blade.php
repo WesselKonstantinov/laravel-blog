148e293505509390674571d0437a36859de52618
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-09-27 20:37:03 +0200

    Update blade syntax

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,16 +4,16 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <?php foreach ($posts as $post) : ?>
-        <article>
-            <h1>
-                <a href="/posts/<?= $post->slug; ?>">
-                    <?= $post->title; ?>
-                </a>
-            </h1>
-            <div>
-                <?= $post->excerpt; ?>
-            </div>
-        </article>
-    <?php endforeach; ?>
+    @foreach ($posts as $post)
+    <article>
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
+        </h1>
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
+    @endforeach
 </body>
