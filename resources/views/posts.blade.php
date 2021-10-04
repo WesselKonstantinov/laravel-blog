00ab954cc40772ba8e3f00ee1a47ea9e6a77e284
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-04 21:26:03 +0200

    Refer to user as author

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,7 +9,7 @@
         </a>
     </h1>
     <p>
-        <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+       By <a href="#">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
     </p>
     <div>
         {{ $post->excerpt }}
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->with('category')->get()
+        'posts' => Post::latest()->with('category', 'author')->get()
     ]);
 });
 
