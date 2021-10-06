47cfd9ccb8c42ecc6da81becf9a3477e7a4a4c91
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-06 21:08:02 +0200

    Reorganize layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,5 @@
-@extends('layout')
-
-@section('content')
-@foreach ($posts as $post)
+<x-layout>
+<!-- @foreach ($posts as $post)
 <article>
     <h1>
         <a href="/posts/{{ $post->slug }}">
@@ -15,5 +13,21 @@
         {{ $post->excerpt }}
     </div>
 </article>
-@endforeach
-@endsection
+@endforeach -->
+    @include ('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+           <x-post-card />
+           <x-post-card />
+           <x-post-card />
+        </div>
+    </main>
+</x-layout>
